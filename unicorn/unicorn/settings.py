1e04ed2e705d1800d134e8eb3b8ce642a05bbef5
sh <nshh9069@gmail.com>
2023-08-25 23:17:02 +0900

    feat : db setting

--- unicorn/unicorn/settings.py
+++ unicorn/unicorn/settings.py
@@ -123,9 +123,13 @@ WSGI_APPLICATION = "unicorn.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'unicorn',
+        'USER': 'sh',
+        'PASSWORD': 'seok4591',
+        'HOST': 'mutsa-db.cszfp3a4or6x.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
